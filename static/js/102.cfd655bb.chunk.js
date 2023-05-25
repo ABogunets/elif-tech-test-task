798ad699ac2f26550235f0d08c577a670503b60f
ABogunets <abogunets@gmail.com>
2023-05-26 00:48:49 +0300

    Updates

---
+++ static/js/102.cfd655bb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkelif_tech_test_task=self.webpackChunkelif_tech_test_task||[]).push([[102],{102:function(t,n,e){e.r(n),e.d(n,{default:function(){return r}});var c,u=e(168),s=e(444).ZP.div(c||(c=(0,u.Z)(["\n  padding: 20px 30px;\n  /* background-color: #90c6f2; */\n"]))),f=e(184),r=function(){return(0,f.jsx)(s,{})}}}]);
+//# sourceMappingURL=102.cfd655bb.chunk.js.map
