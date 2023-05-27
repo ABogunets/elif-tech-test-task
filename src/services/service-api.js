4e1f1d4e46d0f886b118ae1b5f2217707e2505a8
ABogunets <abogunets@gmail.com>
2023-05-27 13:08:54 +0300

    ACTION: number of dishes set to 10 at backend

--- src/services/service-api.js
+++ src/services/service-api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://646e1a959c677e23218b15ff.mockapi.io";
 
 export const fetchDishes = async (page) => {
-  const response = await axios.get(`/dishes?limit=5&page=${page}`);
+  const response = await axios.get(`/dishes?limit=10&page=${page}`);
   return response.data;
 };
 
